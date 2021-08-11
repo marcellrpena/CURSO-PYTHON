631bfb17814a02d2662d0d5ef6c829abb9152445
Marcell Rocha Pena <88504860+marcellrpena@users.noreply.github.com>
2021-08-11 10:36:46 -0300

    Update DESAFIO013.py
    alteração na variavel de decisão de "n" para 'e'

--- PYCHARM_PROJECTS/DESAFIO013.py
+++ PYCHARM_PROJECTS/DESAFIO013.py
@@ -1,23 +1,23 @@
-n2 = '1'
-while n2 =='1':
+e2 = '1'
+while e2 =='1':
     print('Este programa calcula o novo salario \ndo seu funcionário com o novo reajuste:')
     salario = float(input('Digite o valor do seu salário: '))
-    n3 = input('Para um reajuste positivo digite 1 \nPara um reajuste negativo digite 0: ')
-    while not (n3=='1' or n3=='0'):
-        n3 = input('Você digitou a opção errada, tente novamente: \nPara um reajuste positivo digite 1 \nPara um reajuste negativo digite 0 : ')
-    if n3 == '1':
+    e3 = input('Para um reajuste positivo digite 1 \nPara um reajuste negativo digite 0: ')
+    while not (e3=='1' or e3=='0'):
+        e3 = input('Você digitou a opção errada, tente novamente: \nPara um reajuste positivo digite 1 \nPara um reajuste negativo digite 0 : ')
+    if e3 == '1':
         reajuste = float(input('Digite a porcentagem do aumento salarial:'))
         reajuste = salario*reajuste/100
         n_salario = salario + reajuste
-    if n3 == '0':
+    if e3 == '0':
         reajuste = float(input('Digite a porcentagem do decremento salarial:'))
         reajuste = salario * reajuste / 100
         n_salario = salario - reajuste
     print('O seu antigo salário era: {} \nSeu novo salário é: {}'.format(salario,n_salario))
-    n2 = input('digite 1 para continuar e 0 para sair:')
-    if n2=='1':
+    e2 = input('digite 1 para continuar e 0 para sair:')
+    if e2=='1':
         print('\n     \n    \n   ')
-    if n2 == '0':
+    if e2 == '0':
         print('Muito obrigado por utilizar o nosso serviço!!')
-    while not (n2 == '1' or n2 == '0'):
-        n2 = input('opção errada, digite 1 para continuar e 0 para sair:')
+    while not (e2 == '1' or e2 == '0'):
+        e2 = input('opção errada, digite 1 para continuar e 0 para sair:')
